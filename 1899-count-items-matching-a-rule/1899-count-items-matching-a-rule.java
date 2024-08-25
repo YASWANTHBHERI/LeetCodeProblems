2d5c120ff721645dfb42feaff35c3987499efd24
YASWANTHBHERI <129360841+YASWANTHBHERI@users.noreply.github.com>
2024-08-25 11:16:54 +0530

    Time: 6 ms (37.53%), Space: 50.8 MB (22.44%) - LeetHub

--- 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.java
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.java
@@ -1,19 +1,19 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int count = 0;
-        for(int i=0;i<1;i++){
-            for(int j=0;j<items.size();j++){
-                String type = items.get(j).get(0);
-                String color = items.get(j).get(1);
-                String name = items.get(j).get(2);
-                
-                if(ruleKey.equals("type")){
-                    if(ruleValue.equals(type))count++;
-                }else if(ruleKey.equals("color")){
-                    if(ruleValue.equals(color))count++;
-                }else if(ruleKey.equals("name")){
-                    if(ruleValue.equals(name))count++;
-                }
+        for(int i=0;i<items.size();i++){
+            switch(ruleKey){
+                case "type":
+                    if(ruleValue.equals(items.get(i).get(0)))count++;
+                    break;
+                case "color":
+                    if(ruleValue.equals(items.get(i).get(1)))count++;
+                    break;
+                case "name":
+                    if(ruleValue.equals(items.get(i).get(2)))count++;
+                    break;
+                default:
+                    break;
             }
         }
         return count;
