b8dd3b0ee52e13fa70b56cd81647a7dc5d63d3d9
YASWANTHBHERI <bheriyaswanth@gmail.com>
2024-11-14 11:27:47 +0530

    Time: 1 ms (98.9%), Space: 43.2 MB (59.87%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,62 +1,21 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        
-        Map<Integer,Integer> hmap1 = new HashMap<>();
-        Map<Integer,Integer> hmap2 = new HashMap<>();
+        int[] freqArr = new int[1001];
 
-        for(int i=0;i<nums1.length;i++){
-            if(!hmap1.containsKey(nums1[i])){
-                hmap1.put(nums1[i],1);
-            }else {
-                hmap1.put(nums1[i],hmap1.get(nums1[i])+1);
-                nums1[i] = -1;
+        for(int num: nums1){
+            freqArr[num]++;
+        }
+        ArrayList<Integer>ans = new ArrayList<>();
+        for(int num: nums2){
+            if(freqArr[num]>0){
+                ans.add(num);
+                freqArr[num]--;
             }
         }
-
-        for(int i=0;i<nums2.length;i++){
-            if(!hmap2.containsKey(nums2[i])){
-                hmap2.put(nums2[i],1);
-            }else{
-                hmap2.put(nums2[i],hmap2.get(nums2[i])+1);
-                nums2[i] = -1;   
-            }
-        }
-
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            if(nums1[i]!=-1 && hmap2.containsKey(nums1[i])){
-                int count1 = hmap1.get(nums1[i]);
-                int count2 = hmap2.get(nums1[i]);
-
-                if(count1>count2){
-                    int sum = Math.min(count1,count2);
-                    while(sum>0){
-                        ans.add(nums1[i]);
-                        sum--;
-                    }
-                }else if(count1<count2){
-                    int sum = Math.min(count1,count2);
-                    while(sum>0){
-                        ans.add(nums1[i]);
-                        sum--;
-                    }
-                }
-                else{
-                    int sum = count1;
-                    while(sum>0){
-                        ans.add(nums1[i]);
-                        sum--;
-                    }
-                }
-            }
-        }
-
-        int[] res = new int[ans.size()];
-        for(int i=0;i<res.length;i++){
+        int [] res = new int[ans.size()];
+        for(int i=0;i<ans.size();i++){
             res[i] = ans.get(i);
         }
         return res;
-
-
     }
 }
