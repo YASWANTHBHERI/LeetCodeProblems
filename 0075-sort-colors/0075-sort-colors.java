0a08560d3a47f3a44f9b027e902e8cd620f085ad
YASWANTHBHERI <bheriyaswanth@gmail.com>
2024-12-30 10:34:30 +0530

    Time: 0 ms (100%), Space: 42 MB (34.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,36 @@
 class Solution {
     public void sortColors(int[] nums) {
-      
-     int l = 0;
-     int r = nums.length-1;
-     int m = 0;
+        int low = 0;
+        int high = nums.length-1;
+        sort(nums,low,high);
+    }
+    public void sort(int[]nums,int low, int high){
+        if(low<high){
+            int pIndex = partition(nums,low, high);
+            sort(nums,low, pIndex-1);
+            sort(nums,pIndex+1,high);
 
-     while(m<=r)
-     {
-        if(nums[m]==0)
-        {
-            int temp = nums[m];
-            nums[m] = nums[l];
-            nums[l] = temp;
-            m++;
-            l++;
         }
-        else if(nums[m]==1) m++;
-        else if(nums[m]==2){
-            int temp = nums[m];
-            nums[m] = nums[r];
-            nums[r] = temp;
-            r--;
+    }
+    public int partition(int[] nums,int low, int high){
+        int pivot = low;
+        int i = low;
+        int j = high;
+        while(i<j){
+            while(nums[i]<=nums[pivot] && i<=high-1){
+                i++;
+            }
+            while(nums[j]>nums[pivot] && j>=low+1){
+                j--;
+            }
+            if(i<j) swap(nums,i,j);
         }
-        }
+        swap(nums,pivot,j);
+        return j;
+    }
+    public void swap(int[] nums, int firstIndex, int secondIndex){
+        int temp = nums[firstIndex];
+        nums[firstIndex] = nums[secondIndex];
+        nums[secondIndex] = temp;
     }
 }
